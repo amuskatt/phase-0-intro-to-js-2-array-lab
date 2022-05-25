504201bc4aa8856125e64f8b4b2498c8ff8e2624
amuskatt <102235034+amuskatt@users.noreply.github.com>
2022-05-25 17:06:28 -0400

    commit tree 9487dd762f0364febc5517197f80361f3fed0825

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat (Ralph) {
+cats.push("Ralph");
+}
+
+function destructivelyPrependCat (Bob) {
+    cats.unshift ("Bob");
+}
+
+function destructivelyRemoveLastCat () {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat () {
+    cats.shift ();
+}
+
+function appendCat (Broom) {
+    const soManyCats = [...cats, "Broom"]
+    return soManyCats
+}
+
+function prependCat (Arnold) {
+    const moreCats = ["Arnold", ...cats]
+    return moreCats
+}
+
+function removeLastCat () {
+   return cats.slice(0, 2);
+   
+  }
+
+  function removeFirstCat () {
+      return cats.slice(1);
+  }
